755990acc0ea1caf97ea9c18db5b46ecfc012a9e
lash <dev@holbrook.no>
2023-08-12 09:15:42 +0100

    Add fs output format, reverse index by hash for resolve

--- python/eth_bb/filter/fs.py
+++ python/eth_bb/filter/fs.py
@@ -18,13 +18,22 @@ class Filter(MemFilter):
         self.p = None
 
 
+    def prepare(self, ctx):
+        super(Filter, self).prepare(ctx)
+        idx = ctx['usr'].get('bbindex')
+        if idx == 'fs':
+            self.resolve_index_push = self.resolve_index_push_fs
+            self.resolve_index_process = self.resolve_index_process_fs
+
+
     def connect_store(self, ctx):
         self.p = ctx['usr'].get('bbpath')
         dp = os.path.join(self.p, '.resolve')
         os.makedirs(dp, exist_ok=True)
-     
+    
 
-    def add(self, time, author, topic, hsh, ctx):
+    def resolve_index_push_fs(self, time, author, topic, hsh, ctx):
+        logg.debug('push fs')
         dp = os.path.join(self.p, '.resolve')
         os.makedirs(dp, exist_ok=True)
         fp = os.path.join(dp, hsh)
@@ -35,22 +44,9 @@ class Filter(MemFilter):
         f = open(fp, 'a')
         f.write(data)
         f.close()
-        super(Filter, self).add(time, author, topic, hsh, ctx) 
 
 
-    def store_item_for(self, author, topic, time, content, hsh):
-        logg.debug('store item for {} {} {} {}'.format(time, author, topic, hsh))
-        dp = os.path.join(self.p, author, topic)
-        os.makedirs(dp, exist_ok=True)
-
-        fp = os.path.join(dp, time)
-        f = open(fp, 'a')
-        f.write(content)
-        f.write("\n")
-        f.close()
-
-
-    def store_item(self, content, hsh):
+    def resolve_index_process_fs(self, content, hsh):
         fp = os.path.join(self.p, '.resolve', hsh)
         f = open(fp, 'r')
         i = 0
@@ -62,6 +58,31 @@ class Filter(MemFilter):
             topic = r[:64]
             author = r[64:64+40]
             time = r[64+40:]
-            self.store_item_for(author, topic, time, content, hsh)
+            self.store_item_for(time, author, topic, content, hsh)
         f.close()
         os.unlink(fp)
+
+
+    def add(self, time, author, topic, hsh, ctx):
+        self.resolve_index_push(time, author, topic, hsh, ctx)
+        self.resolve(hsh)
+
+
+    def format(self, time, author, topic, content, hsh):
+        return '[{}]\n\n{}\n---\n\n'.format(time, content)
+
+
+    def store_item_for(self, time, author, topic, content, hsh):
+        logg.debug('store item for {} {} {} {}'.format(time, author, topic, hsh))
+        dp = os.path.join(self.p, author, topic)
+        os.makedirs(dp, exist_ok=True)
+
+        data = self.format(time, author, topic, content, hsh)
+        fp = os.path.join(dp, time)
+        f = open(fp, 'a')
+        f.write(data)
+        f.close()
+
+
+    def store_item(self, content, hsh):
+        self.resolve_index_process(content, hsh)
--- python/eth_bb/filter/mem.py
+++ python/eth_bb/filter/mem.py
@@ -13,8 +13,7 @@ class Filter(BaseFilter):
     def __init__(self):
         super(Filter, self).__init__()
         self.contents = {}
-        self.reverse_author = {}
-        self.reverse_topic = {}
+        self.reverse = {}
 
     
     def add(self, time, author, topic, hsh, ctx):
@@ -24,9 +23,25 @@ class Filter(BaseFilter):
             self.contents[author] = {
                     topic: [],
                     }
-        self.reverse_author[hsh] = author
-        self.reverse_topic[hsh] = topic
-        self.resolve(hsh)
+        self.resolve_index_push(time, author, topic, hsh, ctx)
+
+
+    def resolve_index_push(self, time, author, topic, hsh, ctx):
+        time = time.strftime("%Y%m%d")
+        o = (topic, author, time,)
+        self.reverse[hsh].append(o)
+
+
+    def resolve_index_process(self, content, hsh):
+        while True:
+            r = None
+            try:
+                r = self.reverse[hsh].pop()
+            except IndexError:
+                break
+            except KeyError:
+                break
+            self.store_item_for(r[1], r[0], r[2], content, hsh)
 
 
     def store_item(self, content, hsh):
